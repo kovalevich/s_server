58fabd31e7c1302ab635ff5067b4e1e48c6bf142
Yauhen Kavalevich <yauhen.kavalevich@profitero.com>
2024-05-02 10:13:58 +0300

    ready

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync"
+)
+
+var (
+	requests []string
+	mu       sync.Mutex
 )
 
 func main() {
@@ -14,12 +21,51 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", HelloHandler)
-
+	http.HandleFunc("/api/add", handleAdd)
+	http.HandleFunc("/api/get", handleGet)
 	log.Println("Listening on port", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
-func HelloHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Hello from Koyeb\n")
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+	requests = append(requests, string(body))
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	response := struct {
+		Requests []string `json:"requests"`
+	}{
+		Requests: requests,
+	}
+
+	requests = nil // Clear the array after getting its contents
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		return
+	}
 }
